55ba99d1b3c62bbd3793f8d4bc1a0b88260ac29f
julialiuliu <jiuboliu@gmail.com>
2018-03-24 07:50:22 +1300

    Update steps.py

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,14 +1,14 @@
-import behave
 import os
 import sys
 
-from time import sleep
 from appium import webdriver
+from behave import given, when, then, step
+from time import sleep
 
 PAGE_NAVIGATION_TIMER    = 5
 IDLE_TIMER               = 3
 
-@behave.given('the app is launched')
+@given('the app is launched')
 def step_impl(context):
     try:
         context.reddit_home_page.skip_on_board_page()
@@ -18,23 +18,23 @@ def step_impl(context):
     finally:
         sleep(PAGE_NAVIGATION_TIMER)
 
-@behave.when(u'"{searchedterm}" is entered')
+@when(u'"{searchedterm}" is entered')
 def step_impl(context, searchedterm):
     context.reddit_home_page.input_searched_term(searchedterm)
     sleep(IDLE_TIMER)
     context.reddit_home_page.send_enter_key()
     sleep(PAGE_NAVIGATION_TIMER)
 
-@behave.then(u'should be able to see the "{searchedterm}" results')
+@then(u'should be able to see the "{searchedterm}" results')
 def step_impl(context, searchedterm):
     assert context.reddit_home_page.check_searched_result_is_displayed(searchedterm) is True
     sleep(IDLE_TIMER)
 
-@behave.when(u'tap on the "{searchedterm}" in the searched result list')
+@when(u'tap on the "{searchedterm}" in the searched result list')
 def step_impl(context, searchedterm):
     context.reddit_home_page.tap_on_the_searched_result(searchedterm)
     sleep(PAGE_NAVIGATION_TIMER)
 
-@behave.then('should be able to save the most recent posted title')
+@then('should be able to save the most recent posted title')
 def step_impl(context):
     assert context.reddit_home_page.save_top_posted_title_on_subreddit() is True
