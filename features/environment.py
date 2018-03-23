6d2f7a10d84fca3bd851af05ac16fe11b05cedd3
julialiuliu <jiuboliu@gmail.com>
2018-03-23 22:24:31 +1300

    Submit the initial version

---
+++ features/environment.py
@@ -0,0 +1,65 @@
+import os
+import sys
+
+from appium import webdriver
+from time import sleep
+from utils.smart_driver import *
+
+#Hooks
+def before_all(context):
+    context.config.setup_logging()
+    driver_setup(context.config.userdata.get("appium_host", "unknown"),
+                 context.config.userdata.get("appium_port", "unknown"),
+                 context.config.userdata.get("appium_version", "unknown"),
+                 context.config.userdata.get("platform_name", "unknown"),
+                 context.config.userdata.get("platform_version", "unknown"),
+                 context.config.userdata.get("device_name", "unknown"),
+                 context.config.userdata.get("browser_name", "unknown"),
+                 context.config.userdata.get("device_orientation", "unknown"),
+                 context.config.userdata.get("test_name", "unknown"),
+                 context.config.userdata.get("app_uri", "unknown"),
+                 context.config.userdata.get("saucelabs_user", "unknown"),
+                 context.config.userdata.get("saucelabs_key", "unknown"),
+                 context.config.userdata.get("saucelabs_on", "unknown"))
+    start_driver(context)
+
+def after_all(context):
+    pass
+    #uninstall the app on cloude device
+    #context.driver.remove_app(context.config.userdata.get("app_uri"));
+
+def before_feature(context, feature):
+    #launch_app()
+    pass
+
+def before_scenario(context, scenario):
+    #start_activity()
+    #Android ONLY
+    #get_current_activity()
+    pass
+
+def after_scenario(context, scenario):
+    if scenario.status == 'failed':
+       pass
+
+def after_feature(context, feature):
+    cleanup_driver(context)
+
+def before_step(context, step):
+    pass
+
+def after_step(context, step):
+    take_screenshot(context, step.name)
+    if step.status == "failed":
+        allure.attach(context.driver.get_screenshot_as_file('screenshots\\{}.png'.format
+                       (step.name)),
+        name="Screenshot",
+        attachment_type=AttachmentType.PNG)
+        sleep(IDLE_TIMER)
+    pass
+
+def before_tag(context, tag):
+    pass
+
+def after_tag(context, tag):
+    pass
---
+++ features/steps/steps.py
@@ -0,0 +1,43 @@
+import behave
+import os
+import sys
+
+from time import sleep
+from appium import webdriver
+
+PAGE_NAVIGATION_TIMER    = 5
+IDLE_TIMER               = 3
+
+@behave.given('the app is launched')
+def step_impl(context):
+    try:
+        context.reddit_home_page.skip_on_board_page()
+        return
+    except Exception as e:
+        raise e
+    finally:
+        sleep(PAGE_NAVIGATION_TIMER)
+    pass
+
+@behave.when(u'"{searchedterm}" is entered')
+def step_impl(context, searchedterm):
+    context.reddit_home_page.input_searched_term(searchedterm)
+    sleep(IDLE_TIMER)
+    context.reddit_home_page.send_enter_key()
+    sleep(PAGE_NAVIGATION_TIMER)
+
+@behave.then(u'should be able to see the "{searchedterm}" results')
+def step_impl(context, searchedterm):
+    assert context.reddit_home_page.check_searched_result_is_displayed(searchedterm) is True
+    sleep(IDLE_TIMER)
+
+@behave.when(u'tap on the "{searchedterm}" in the searched result list')
+def step_impl(context, searchedterm):
+    context.reddit_home_page.tap_on_the_searched_result(searchedterm)
+    sleep(PAGE_NAVIGATION_TIMER)
+    pass
+
+@behave.then('should be able to save the most recent posted title')
+def step_impl(context):
+    assert context.reddit_home_page.save_top_posted_title_on_subreddit() is True
+    pass
